f5da59f83b5e0759dd34c04495487ac17a158837
feriosch <47795404+feriosch@users.noreply.github.com>
2019-03-29 18:07:57 -0600

    Ya logre hacer el fade, y tambien le puse la contraseña

--- venv/Main/static/js/login.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-  var x = document.forms["login_form"]["usuario"].value;
-  if (x == "") {
-      var popup = document.getElementById("usuario_vacio");
-      popup.classList.toggle("show");
-      return false;
-  }
-}
